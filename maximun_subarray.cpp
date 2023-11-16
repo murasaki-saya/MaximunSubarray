442099bedf534eb9ddbfd80b06178448ea206718
murasaki saya(He-Teng Shih) <91206239+murasaki-saya@users.noreply.github.com>
2023-11-16 19:18:46 +0800

    Add files via upload

--- maximun_subarray.cpp
+++ maximun_subarray.cpp
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main (void){
-	int size,state=-1,total=0,sum[2];
+	int size,total=0,sum[2];
 	sum[0]=0, sum[1]=0;
-	bool start=false;
+	bool start=false, state=false, sum1 = false;
 	printf("input size: ");
 	scanf("%d", &size);
 	int num[size];
@@ -13,7 +13,7 @@ int main (void){
 	}
 	
 	for(int i=0 ; i<size ; i++){
-		//find the first positive number
+		//尋找起始正數 
 		if(num[i] > 0){
 			start = true;
 		}
@@ -24,25 +24,27 @@ int main (void){
 		
 		if(!i)
 			continue;
-		//from (positive to negative / negative to positive)	
-		if(num[i-1]<0 && num[i]>=0)
-			state=1;
-		else if(num[i-1]>=0 && num[i]<0)
-			state=0;
- 
-		if(state==1)
-			sum[1] += num[i]; 
-		else if(!state){	//max{sum[0],total,sum[1]}
+		//由正轉負 或 由負轉正	
+		if(num[i-1]<0 && num[i]>=0){
+			sum1=true;
+		}else if(num[i-1]>=0 && num[i]<0)
+			state=true;
+  
+		if(state){	//由正轉負要保存目前最佳解 max{sum[0],total,sum[1]}
 			printf("total: %d, result: %d, sum[1]: %d\n",total,sum[0],sum[1]);
 			if(total-num[i] >= sum[0])
 				sum[0] = total-num[i];
-			if(sum[1] >= sum[0]){
-				total = sum[1]+num[i];
+			if(sum[1]>=sum[0]){
+				total = sum[1];
 				sum[0] = sum[1];
+				sum[1] = 0;
+				sum1 = false;
 			}
-			sum[1] = 0;
-			state = -1;
+			state = false;
 		}
+		
+		if(sum1)
+			sum[1] += num[i];
 	}
 	if(total > sum[0])
 		sum[0] = total;
